0267b7bf539aa6e387073e3b06680bc6a1ff74e9
Mick Kanchanachayphoom <mick@MacBook-Pro.local>
2021-10-24 00:10:22 -0500

    LOTS OF STUFF HERE

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
+import Circle from './Circle.js'
+import './Circle.css';
 
 class App extends React.Component {
 
@@ -8,7 +10,7 @@ class App extends React.Component {
     super(props);
 
     let strIndex = 0;
-    let textToRead = "Hello";
+    let textToRead = "hello";
 
     this.state = {
       strIndex: strIndex,
@@ -16,8 +18,106 @@ class App extends React.Component {
       currentChar: textToRead[strIndex],
       currentX: 0,
       currentY: 0,
-      mouseDown: "TRUE", 
+      mouseDown: true,
+      posx: [0, 0, 0, 0, 0, 0],
+      posy: [0, 0, 0, 0, 0, 0],
+      width: 0,
+      height: 0,
+      'a': [true, false, false, false, false, false],
+      'b': [true, true, false, false, false, false],
+      'c': [true, false, false, true, false, false],
+      'd': [true, false, false, true, true, false],
+      'e': [true, false, false, false, true, false],
+      'f': [true, true, false, true, false, false],
+      'g': [true, true, false, true, true, false],
+      'h': [true, true, false, false, true, false],
+      'i': [false, true, false, true, false, false],
+      'j': [false, true, false, true, true, false],
+      'k': [true, false, true, false, false, false],
+      'l': [true, true, true, false, false, false],
+      'm': [true, false, true, true, false, false],
+      'n': [true, false, true, true, true, false],
+      'o': [true, false, true, false, true, false],
+      'p': [true, true, true, true, false, false],
+      'q': [true, true, true, true, true, false],
+      'r': [true, true, true, false, true, false],
+      's': [false, true, true, true, false, false],
+      't': [false, true, true, true, true, false],
+      'u': [true, false, true, false, false, true],
+      'v': [true, true, true, false, false, true],
+      'w': [false, true, false, true, true, true],
+      'x': [true, false, true, true, false, true],
+      'y': [true, false, true, true, true, true],
+      'z': [true, false, true, false, true, true],
+      '1a': [false, false, true, true, true, true],
+      '1b': [true, false, false, false, false, false],
+      '2a': [false, false, true, true, true, true],
+      '2b': [true, true, false, false, false, false],
+      '3a': [false, false, true, true, true, true],
+      '3b': [true, false, false, true, false, false],
+      '4a': [false, false, true, true, true, true],
+      '4b': [true, false, false, true, true, false],
+      '5a': [false, false, true, true, true, true],
+      '5b': [true, false, false, false, true, false],
+      '6a': [false, false, true, true, true, true],
+      '6b': [true, true, false, true, false, false],
+      '7a': [false, false, true, true, true, true],
+      '7b': [true, true, false, true, true, false],
+      '8a': [false, false, true, true, true, true],
+      '8b': [true, true, false, false, true, false],
+      '9a': [false, false, true, true, true, true],
+      '9b': [false, true, false, true, false, false],
+      '0a': [false, false, true, true, true, true],
+      '0b': [false, true, false, true, true, false],
+      ',': [false, true, false, false, false, false],
+      ';': [false, true, true, false, false, false],
+      ':': [false, true, false, false, true, false],
+      '.': [false, true, false, false, true, true],
+      '?': [false, true, true, false, false, true],
+      '!': [false, true, true, false, true, false],
     }
+  }
+
+  componentDidMount() {
+    let elem0 = document.getElementById("btn0");
+    let btn0 = elem0.getBoundingClientRect();
+    let x0 = btn0.x;
+    let y0 = btn0.y;
+    let width0 = elem0.offsetWidth;
+    let height0 = elem0.offsetHeight;
+
+    let elem1 = document.getElementById("btn1");
+    let btn1 = elem1.getBoundingClientRect();
+    let x1 = btn1.x;
+    let y1 = btn1.y;
+
+    let elem2 = document.getElementById("btn2");
+    let btn2 = elem2.getBoundingClientRect();
+    let x2 = btn2.x;
+    let y2 = btn2.y;
+
+    let elem3 = document.getElementById("btn3");
+    let btn3 = elem3.getBoundingClientRect();
+    let x3 = btn3.x;
+    let y3 = btn3.y;
+
+    let elem4 = document.getElementById("btn4");
+    let btn4 = elem4.getBoundingClientRect();
+    let x4 = btn4.x;
+    let y4 = btn4.y;
+
+    let elem5 = document.getElementById("btn5");
+    let btn5 = elem5.getBoundingClientRect();
+    let x5 = btn5.x;
+    let y5 = btn5.y;
+
+    this.setState({
+      ...this.state,
+      width: width0,
+      height: height0,
+      posx: [x0, x1, x2, x3, x4, x5],
+      posy: [y0, y1, y2, y3, y4, y5]
+    })
   }
 
   _onMouseMove(e) {
@@ -30,27 +130,16 @@ class App extends React.Component {
 
   handleEvent = (event) => {
     if (event.type === "pointerdown") {
-      // this.setState({ message: "Mouse Down"});
-      // this.setState({message: "Mouse Down"});
-      // console.log("DOWN!")
       this.setState({
         ...this.state,
-        mouseDown: "TRUE",
-        // currentX: event.screenX, 
-        // currentY: event.screenY
+        mouseDown: true,
       })
-      let elem = document.getElementById("button1");
-      let btn1 = elem.getBoundingClientRect();
-      console.log(elem.offsetHeight)
-      console.log(elem.offsetWidth)
-      console.log(btn1.x + " " + btn1.y)
     } else {
-        // this.setState({ message: "Mouse Up"});
-      // this.setState({message: "Mouse Up"})
-      // console.log("UP!")
       this.setState({
         ...this.state,
-        mouseDown: "FALSE",
+        mouseDown: false,
+        currentX: 0,
+        currentY: 0,
       })
     }
   }
@@ -67,16 +156,16 @@ class App extends React.Component {
     }
   }
 
-  // onClickHandler = event => {
-  //   clearTimeout(timer);
-  //   if (event.detail === 1) {
-  //       timer = setTimeout(this.props.onClick, 200)
-  //       console.log("1")
-  //   } else if (event.detail === 2) {
-  //       this.props.onDoubleClick()
-  //       console.log("2")
-  //   }
-  // }
+  handleVibrate = (ind) => {
+    let currentChar = this.state.currentChar;
+    let parseChar = parseInt(currentChar);
+
+    if (isNaN(parseChar)) {
+      return this.state[this.state.currentChar][ind]
+    } else {
+
+    }
+  }
 
   render() {
     return (
@@ -87,21 +176,22 @@ class App extends React.Component {
         onPointerMove={this._onMouseMove.bind(this)}>
         <div>
           {/* <Circle vibrate={true} currentX={this.state.currentX} currentY={this.state.currentY} posX poxY></Circle> */}
-          <button id="button1">Hello1</button>
-          <button>Hello4</button>
+          {/* <button id="button1">Hello1</button>
+          <button>Hello4</button> */}
+          <Circle id="btn0" mouseDown={this.state.mouseDown} vibrate={this.handleVibrate(0)} currentX={this.state.currentX} currentY={this.state.currentY} posx={this.state.posx[0]} posy={this.state.posy[0]} width={this.state.width} height={this.state.height}/> 
+          <Circle id="btn1" mouseDown={this.state.mouseDown} vibrate={this.state[this.state.currentChar][1]} currentX={this.state.currentX} currentY={this.state.currentY} posx={this.state.posx[1]} posy={this.state.posy[1]} width={this.state.width} height={this.state.height}/> 
+          <Circle id="btn2" mouseDown={this.state.mouseDown} vibrate={this.state[this.state.currentChar][2]} currentX={this.state.currentX} currentY={this.state.currentY} posx={this.state.posx[2]} posy={this.state.posy[2]} width={this.state.width} height={this.state.height}/> 
         </div>
         <div>
-          <button>Hello2</button>
-          <button>Hello5</button>
+          <Circle id="btn3" mouseDown={this.state.mouseDown} vibrate={this.state[this.state.currentChar][3]} currentX={this.state.currentX} currentY={this.state.currentY} posx={this.state.posx[3]} posy={this.state.posy[3]} width={this.state.width} height={this.state.height}/> 
+          <Circle id="btn4" mouseDown={this.state.mouseDown} vibrate={this.state[this.state.currentChar][4]} currentX={this.state.currentX} currentY={this.state.currentY} posx={this.state.posx[4]} posy={this.state.posy[4]} width={this.state.width} height={this.state.height}/> 
+          <Circle id="btn5" mouseDown={this.state.mouseDown} vibrate={this.state[this.state.currentChar][5]} currentX={this.state.currentX} currentY={this.state.currentY} posx={this.state.posx[5]} posy={this.state.posy[5]} width={this.state.width} height={this.state.height}/> 
         </div>
-        <div>
-          <button>Hello3</button>
-          <button>Hello6</button>
-        </div>
-        <div>{this.state.strIndex}</div>
+        {/* <div>{this.state.strIndex}</div>
         <div>{this.state.currentChar}</div>
         <div>x: {this.state.currentX} y: {this.state.currentY}</div>
         <div>{this.state.mouseDown}</div>
+        <div>{this.state['a']}</div> */}
       </div>
     );
   }
--- src/Circle.js
+++ src/Circle.js
@@ -1,19 +1,49 @@
 import React, {useState} from "react";
+import './Circle.css'
 
 class Circle extends React.Component {
     render(){
-        const [vibrate, currentX, currentY, posx, posy, width, height] = this.props;
+        const vibrate = this.props.vibrate;
+        const currentX = this.props.currentX;
+        const currentY = this.props.currentY;
+        const posx = this.props.posx;
+        const posy = this.props.posy;
+        const width = this.props.width;
+        const height = this.props.height;
+        const id = this.props.id;
+        const mouseDown = this.props.mouseDown;
        
         if(vibrate){
-            if(posx <= currentX  && currentX<= posx+width && posy <= currentY && currentY <= posy+height){
+            if(posx <= currentX  && currentX<= posx+width && posy <= currentY && currentY <= posy+height && mouseDown){
                 window.navigator.vibrate(200);
+                console.log("VIBRATED!")
             }
             return (
-                <div class="ActiveCircle" ></div>
+                <div 
+                    className="ActiveCircle"
+                    id={id}
+                    style={{background: "#000000",
+                        borderRadius: "50%",
+                        width: 150,
+                        height: 150,
+                        margin: 20,
+                        borderStyle: "solid",
+                        borderColor: "black",
+                        position: "relative"}}></div>
             );
         }else{
             return (
-                <div class="InActiveCircle"></div>
+                <div 
+                    className="InActiveCircle"
+                    id={id}
+                    style={{background: "#FFFFFF",
+                    borderRadius: "50%",
+                    width: 150,
+                    height: 150,
+                    margin: 20,
+                    borderStyle: "solid",
+                    borderColor: "black",
+                    position: "relative"}}></div>
             );
         }
     }
